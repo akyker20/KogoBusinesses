9e123d1ec5c969e6d21deb0ecb6f026aac8028f7
Austin Kyker <amk66@duke.edu>
2014-11-20 11:00:39 -0500

    added youtube api code to add student video

--- kogo/static/js/companies.js
+++ kogo/static/js/companies.js
@@ -25,3 +25,38 @@ var resetPaddingOfIntroSectionToFitBrowserHeight = function() {
 	var correctPadding = screenHeight/2 - paddingOffset;
 	$("section.intro").css("padding", correctPadding);
 };
+
+var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+    var player;
+    function onYouTubeIframeAPIReady() {
+    	alert("Hello");
+      player = new YT.Player('student-video-player', {
+        height: '390',
+        width: '640',
+        videoId: 'gvJjzYlFcK4',
+        events: {
+          'onStateChange': onPlayerStateChange
+        }
+      });
+    }
+
+    // 5. The API calls this function when the player's state changes.
+    //    The function indicates that when playing a video (state=1),
+    //    the player should play for six seconds and then stop.
+    var done = false;
+    function onPlayerStateChange(event) {
+      if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+      }
+    }
+    function stopVideo() {
+      player.stopVideo();
+    }
